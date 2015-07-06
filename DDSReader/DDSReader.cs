04b6922e9589a26a81d0ca483f26362237c0dcdd
Andrew Burnett <andrew@nospam.forme>
2015-07-06 17:01:00 +0100

    Merge some files

--- DDSReader/DDSReader.cs
+++ DDSReader/DDSReader.cs
@@ -1,24 +1,11 @@
 /*
  * DDSReader
- * Copyright 2006 Michael Farrell
- 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ * Copyright 2006 Michael Farrell 
+ * LGPLv2.1
+ */
 using System.Drawing;
 
-namespace au.id.micolous.libs.DDSReader
+namespace me.andburn.DDSReader
 {
     /// <summary>
     /// This is the main class of the library.  All static methods are contained within.
