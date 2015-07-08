b172cf5cfca0d9b5e5ea3d61dfb819de3ad66e07
Andrew Burnett <andrew@nospam.forme>
2015-07-08 23:16:14 +0100

    Add unit test project

---
+++ DDSReaderTest/SimpleTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using me.andburn.DDSReader;
+using System.IO;
+using System.Drawing;
+
+namespace me.andburn.DDSReaderTest
+{
+	[TestClass]
+	public class SimpleTest
+	{
+		[TestMethod]
+		public void BlackBox()
+		{
+			byte[] data = File.ReadAllBytes(@"E:\Dump\sample.dds");
+			
+			Bitmap bmp = DDSReader.DDSReader.LoadImage(data);
+
+			bmp.Save(@"E:\Dump\dds_sample.png");
+
+			Assert.Fail();
+		}
+	}
+}
