4d82b0632d8689a1b8068371ddcce9de28398246
Andrew Burnett <andrew@nospam.forme>
2015-08-23 17:04:49 +0100

    Add updated decompression

--- DDSReader/Utils.cs
+++ DDSReader/Utils.cs
@@ -1,14 +1,9 @@
-﻿/*
- * DDSReader
- * Copyright 2006 Michael Farrell 
- * LGPLv2.1
- */
-using System;
+﻿using System;
 using System.Runtime.InteropServices;
 
-namespace me.andburn.DDSReader
+namespace me.andburn.DDSReader.Utils
 {
-	public class Utils
+	public class Helper
 	{
 		#region Constants
 		// DDSStruct flags
@@ -65,6 +60,308 @@ namespace me.andburn.DDSReader
 			g = (byte)((temp / 64 + temp) / 64);
 			temp = (color & 0x001F) * 255 + 16;
 			b = (byte)((temp / 32 + temp) / 32);
+		}
+
+		// iCompFormatToBpp
+		public static uint PixelFormatToBpp(PixelFormat pf, uint rgbbitcount)
+		{
+			switch(pf)
+			{
+			case PixelFormat.LUMINANCE:
+			case PixelFormat.LUMINANCE_ALPHA:
+			case PixelFormat.ARGB:
+				return rgbbitcount / 8;
+
+			case PixelFormat.RGB:
+			case PixelFormat.THREEDC:
+			case PixelFormat.RXGB:
+				return 3;
+
+			case PixelFormat.ATI1N:
+				return 1;
+
+			case PixelFormat.R16F:
+				return 2;
+
+			case PixelFormat.A16B16G16R16:
+			case PixelFormat.A16B16G16R16F:
+			case PixelFormat.G32R32F:
+				return 8;
+
+			case PixelFormat.A32B32G32R32F:
+				return 16;
+
+			default:
+				return 4;
+			}
+		}
+
+		// iCompFormatToBpc
+		public static uint PixelFormatToBpc(PixelFormat pf)
+		{
+			switch(pf)
+			{
+			case PixelFormat.R16F:
+			case PixelFormat.G16R16F:
+			case PixelFormat.A16B16G16R16F:
+				return 4;
+
+			case PixelFormat.R32F:
+			case PixelFormat.G32R32F:
+			case PixelFormat.A32B32G32R32F:
+				return 4;
+
+			case PixelFormat.A16B16G16R16:
+				return 2;
+
+			default:
+				return 1;
+			}
+		}
+
+		public static bool Check16BitComponents(DDSStruct header)
+		{
+			if(header.pixelformat.rgbbitcount != 32)
+				return false;
+			// a2b10g10r10 format
+			if(header.pixelformat.rbitmask == 0x3FF00000 && header.pixelformat.gbitmask == 0x000FFC00 && header.pixelformat.bbitmask == 0x000003FF
+				&& header.pixelformat.alphabitmask == 0xC0000000)
+				return true;
+			// a2r10g10b10 format
+			else if(header.pixelformat.rbitmask == 0x000003FF && header.pixelformat.gbitmask == 0x000FFC00 && header.pixelformat.bbitmask == 0x3FF00000
+				&& header.pixelformat.alphabitmask == 0xC0000000)
+				return true;
+
+			return false;
+		}
+
+		public static void CorrectPremult(uint pixnum, ref byte[] buffer)
+		{
+			for(uint i = 0; i < pixnum; i++)
+			{
+				byte alpha = buffer[i + 3];
+				if(alpha == 0) continue;
+				int red = (buffer[i] << 8) / alpha;
+				int green = (buffer[i + 1] << 8) / alpha;
+				int blue = (buffer[i + 2] << 8) / alpha;
+
+				buffer[i] = (byte)red;
+				buffer[i + 1] = (byte)green;
+				buffer[i + 2] = (byte)blue;
+			}
+		}
+
+		public static void ComputeMaskParams(uint mask, ref int shift1, ref int mul, ref int shift2)
+		{
+			shift1 = 0; mul = 1; shift2 = 0;
+			while((mask & 1) == 0)
+			{
+				mask >>= 1;
+				shift1++;
+			}
+			uint bc = 0;
+			while((mask & (1 << (int)bc)) != 0) bc++;
+			while((mask * mul) < 255)
+				mul = (mul << (int)bc) + 1;
+			mask *= (uint)mul;
+
+			while((mask & ~0xff) != 0)
+			{
+				mask >>= 1;
+				shift2++;
+			}
+		}
+
+		public static unsafe void DxtcReadColors(byte* data, ref Colour8888[] op)
+		{
+			byte r0, g0, b0, r1, g1, b1;
+
+			b0 = (byte)(data[0] & 0x1F);
+			g0 = (byte)(((data[0] & 0xE0) >> 5) | ((data[1] & 0x7) << 3));
+			r0 = (byte)((data[1] & 0xF8) >> 3);
+
+			b1 = (byte)(data[2] & 0x1F);
+			g1 = (byte)(((data[2] & 0xE0) >> 5) | ((data[3] & 0x7) << 3));
+			r1 = (byte)((data[3] & 0xF8) >> 3);
+
+			op[0].red = (byte)(r0 << 3 | r0 >> 2);
+			op[0].green = (byte)(g0 << 2 | g0 >> 3);
+			op[0].blue = (byte)(b0 << 3 | b0 >> 2);
+
+			op[1].red = (byte)(r1 << 3 | r1 >> 2);
+			op[1].green = (byte)(g1 << 2 | g1 >> 3);
+			op[1].blue = (byte)(b1 << 3 | b1 >> 2);
+		}
+
+		public static void DxtcReadColor(ushort data, ref Colour8888 op)
+		{
+			byte r, g, b;
+
+			b = (byte)(data & 0x1f);
+			g = (byte)((data & 0x7E0) >> 5);
+			r = (byte)((data & 0xF800) >> 11);
+
+			op.red = (byte)(r << 3 | r >> 2);
+			op.green = (byte)(g << 2 | g >> 3);
+			op.blue = (byte)(b << 3 | r >> 2);
+		}
+
+		public static unsafe void DxtcReadColors(byte* data, ref Colour565 color_0, ref Colour565 color_1)
+		{
+			color_0.blue = (byte)(data[0] & 0x1F);
+			color_0.green = (byte)(((data[0] & 0xE0) >> 5) | ((data[1] & 0x7) << 3));
+			color_0.red = (byte)((data[1] & 0xF8) >> 3);
+
+			color_0.blue = (byte)(data[2] & 0x1F);
+			color_0.green = (byte)(((data[2] & 0xE0) >> 5) | ((data[3] & 0x7) << 3));
+			color_0.red = (byte)((data[3] & 0xF8) >> 3);
+		}
+
+		public static void GetBitsFromMask(uint mask, ref uint shiftLeft, ref uint shiftRight)
+		{
+			uint temp, i;
+
+			if(mask == 0)
+			{
+				shiftLeft = shiftRight = 0;
+				return;
+			}
+
+			temp = mask;
+			for(i = 0; i < 32; i++, temp >>= 1)
+			{
+				if((temp & 1) != 0)
+					break;
+			}
+			shiftRight = i;
+
+			// Temp is preserved, so use it again:
+			for(i = 0; i < 8; i++, temp >>= 1)
+			{
+				if((temp & 1) == 0)
+					break;
+			}
+			shiftLeft = 8 - i;
+
+			return;
+		}
+
+		// This function simply counts how many contiguous bits are in the mask.
+		public static uint CountBitsFromMask(uint mask)
+		{
+			uint i, testBit = 0x01, count = 0;
+			bool foundBit = false;
+
+			for(i = 0; i < 32; i++, testBit <<= 1)
+			{
+				if((mask & testBit) != 0)
+				{
+					if(!foundBit)
+						foundBit = true;
+					count++;
+				}
+				else if(foundBit)
+					return count;
+			}
+
+			return count;
+		}
+
+		public static uint HalfToFloat(ushort y)
+		{
+			int s = (y >> 15) & 0x00000001;
+			int e = (y >> 10) & 0x0000001f;
+			int m = y & 0x000003ff;
+
+			if(e == 0)
+			{
+				if(m == 0)
+				{
+					//
+					// Plus or minus zero
+					//
+					return (uint)(s << 31);
+				}
+				else
+				{
+					//
+					// Denormalized number -- renormalize it
+					//
+					while((m & 0x00000400) == 0)
+					{
+						m <<= 1;
+						e -= 1;
+					}
+
+					e += 1;
+					m &= ~0x00000400;
+				}
+			}
+			else if(e == 31)
+			{
+				if(m == 0)
+				{
+					//
+					// Positive or negative infinity
+					//
+					return (uint)((s << 31) | 0x7f800000);
+				}
+				else
+				{
+					//
+					// Nan -- preserve sign and significand bits
+					//
+					return (uint)((s << 31) | 0x7f800000 | (m << 13));
+				}
+			}
+
+			//
+			// Normalized number
+			//
+			e = e + (127 - 15);
+			m = m << 13;
+
+			//
+			// Assemble s, e and m.
+			//
+			return (uint)((s << 31) | (e << 23) | m);
+		}
+
+		public static unsafe void ConvFloat16ToFloat32(uint* dest, ushort* src, uint size)
+		{
+			uint i;
+			for(i = 0; i < size; ++i, ++dest, ++src)
+			{
+				//float: 1 sign bit, 8 exponent bits, 23 mantissa bits
+				//half: 1 sign bit, 5 exponent bits, 10 mantissa bits
+				*dest = HalfToFloat(*src);
+			}
+		}
+
+		public static unsafe void ConvG16R16ToFloat32(uint* dest, ushort* src, uint size)
+		{
+			uint i;
+			for(i = 0; i < size; i += 3)
+			{
+				//float: 1 sign bit, 8 exponent bits, 23 mantissa bits
+				//half: 1 sign bit, 5 exponent bits, 10 mantissa bits
+				*dest++ = HalfToFloat(*src++);
+				*dest++ = HalfToFloat(*src++);
+				*((float*)dest++) = 1.0f;
+			}
+		}
+
+		public static unsafe void ConvR16ToFloat32(uint* dest, ushort* src, uint size)
+		{
+			uint i;
+			for(i = 0; i < size; i += 3)
+			{
+				//float: 1 sign bit, 8 exponent bits, 23 mantissa bits
+				//half: 1 sign bit, 5 exponent bits, 10 mantissa bits
+				*dest++ = HalfToFloat(*src++);
+				*((float*)dest++) = 1.0f;
+				*((float*)dest++) = 1.0f;
+			}
 		}
 	}
 
