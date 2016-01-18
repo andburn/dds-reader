1f6736320c988e1ec39488989320db3ebbbaa483
Andrew Burnett <andrew@nospam.forme>
2016-01-18 15:01:33 +0000

    Add test images

--- DDSReaderTest/FormatTest.cs
+++ DDSReaderTest/FormatTest.cs
@@ -5,52 +5,59 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AndBurn.DDSReaderTest
 {
-    // Some simple tests, to check setup
-    [TestClass]
-    public class FormatTest
-    {
-        [TestMethod]
-        public void DXT1Supported()
-        {
-            Bitmap bmp = DDS.LoadImage(@"Data\DXT1.dds");
-            Assert.IsNotNull(bmp);
-        }
+	// Some simple tests, to check setup
+	[TestClass]
+	public class FormatTest
+	{
+		[TestMethod]
+		public void DXT1Supported()
+		{
+			Bitmap bmp = DDS.LoadImage(@"Data\sample_dxt1.dds");
+			Assert.IsNotNull(bmp);
+		}
 
-        [TestMethod]
-        public void DXT3Supported()
-        {
-            byte[] data = File.ReadAllBytes(@"Data\DXT3.dds");
-            Bitmap bmp = DDS.LoadImage(data);
-            Assert.IsNotNull(bmp);
-        }
+		[TestMethod]
+		public void DXT3Supported()
+		{
+			byte[] data = File.ReadAllBytes(@"Data\sample_dxt3.dds");
+			Bitmap bmp = DDS.LoadImage(data);
+			Assert.IsNotNull(bmp);
+		}
 
-        [TestMethod]
-        public void DXT5Supported()
-        {
-            Stream stream = File.Open(@"Data\DXT5.dds", FileMode.Open);
-            Bitmap bmp = DDS.LoadImage(stream);
-            Assert.IsNotNull(bmp);
-        }
+		[TestMethod]
+		public void DXT5Supported()
+		{
+			Stream stream = File.Open(@"Data\sample_dxt5.dds", FileMode.Open);
+			Bitmap bmp = DDS.LoadImage(stream);
+			Assert.IsNotNull(bmp);
+		}
 
-        [TestMethod]
-        public void PreserveAlphaChannel_DefaultsTrue()
-        {
-            DDSImage im = new DDSImage(File.Open(@"Data\DXT5_Alpha.dds", FileMode.Open));
-            Assert.IsTrue(im.PreserveAlpha);
-        }
+		[TestMethod]
+		public void PreserveAlphaChannel_DefaultsTrue()
+		{
+			DDSImage im = new DDSImage(File.Open(@"Data\sample_dxt5.dds", FileMode.Open));
+			Assert.IsTrue(im.PreserveAlpha);
+		}
 
-        [TestMethod]
-        public void PreserveAlphaChannel_IsFalse()
-        {
-            DDSImage im = new DDSImage(File.Open(@"Data\DXT5_Alpha.dds", FileMode.Open), false);
-            Assert.IsFalse(im.PreserveAlpha);
-        }
+		[TestMethod]
+		public void PreserveAlphaChannel_IsFalse()
+		{
+			DDSImage im = new DDSImage(File.Open(@"Data\sample_dxt5.dds", FileMode.Open), false);
+			Assert.IsFalse(im.PreserveAlpha);
+		}
 
-        [TestMethod]
-        public void PNGNotSupported()
-        {
-            Bitmap bmp = DDS.LoadImage(@"Data\CRED.png");
-            Assert.IsNull(bmp);
-        }
-    }
+		[TestMethod]
+		public void PNGNotSupported()
+		{
+			Bitmap bmp = DDS.LoadImage(@"Data\sample.png");
+			Assert.IsNull(bmp);
+		}
+
+		[TestMethod]
+		public void BMPNotSupported()
+		{
+			Bitmap bmp = DDS.LoadImage(@"Data\sample.bmp");
+			Assert.IsNull(bmp);
+		}
+	}
 }
