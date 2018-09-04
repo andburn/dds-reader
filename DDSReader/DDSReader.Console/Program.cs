c434a9587743ed71a9ab76a0f92811df27c3a4f9
Andrew Burnett <andrew@andburn.info>
2018-09-04 14:34:19 +0100

    Add exception handling to console

--- DDSReader/DDSReader.Console/Program.cs
+++ DDSReader/DDSReader.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace DDSReader.Console
 {
@@ -6,8 +7,42 @@ namespace DDSReader.Console
 	{
 		static void Main(string[] args)
 		{
-			var dds = new DDSImage(args[0]);
-			dds.Save(args[1]);
+			if (args.Length != 2)
+			{
+				System.Console.WriteLine("ERROR: input and output file required\n");
+				Environment.Exit(1);
+			}
+
+			var input = args[0];
+			var output = args[1];
+
+			if (!File.Exists(input))
+			{
+				System.Console.WriteLine("ERROR: input file does not exist\n");
+				Environment.Exit(1);
+			}
+
+			try
+			{
+				var dds = new DDSImage(input);
+				dds.Save(output);
+			}
+			catch (Exception e)
+			{
+				System.Console.WriteLine("ERROR: failed to convert DDS file\n");
+				System.Console.WriteLine(e);
+				Environment.Exit(1);
+			}
+
+			if (File.Exists(output))
+			{
+				System.Console.WriteLine("Successfully created " + output);
+			}
+			else
+			{
+				System.Console.WriteLine("ERROR: something went wrong!\n");
+				Environment.Exit(1);
+			}
 		}
 	}
 }
